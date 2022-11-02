f7fffad62c7a3c99156367f051b61c786e9dc77f
PRANEETH <98073013+Praneethgpl@users.noreply.github.com>
2022-11-02 14:33:26 +0530

    n=int(input()) a = list(map(int,input().strip().split()))[:n] oddsum=0 evensum=0 for i in range(0,n):     if a[i]%2==0:         oddsum+=a[i]     else:         evensum+=a[i] if oddsum>evensum:     print(oddsum-evensum) else:     print(evensum-oddsum)

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_elements.py
@@ -0,0 +1,13 @@
+n=int(input())
+a = list(map(int,input().strip().split()))[:n]
+oddsum=0
+evensum=0
+for i in range(0,n):
+    if a[i]%2==0:
+        oddsum+=a[i]
+    else:
+        evensum+=a[i]
+if oddsum>evensum:
+    print(oddsum-evensum)
+else:
+    print(evensum-oddsum)
